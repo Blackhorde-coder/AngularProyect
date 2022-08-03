8382af0614e189acbb60512e86454d737bf5b9e9
Hernan Mauricio Pena <esingcomputacion@gmail.com>
2022-08-03 19:09:35 -0300

    Tareas Angular

--- AngularProyect/src/app/app.component.ts
+++ AngularProyect/src/app/app.component.ts
@@ -1,10 +1,36 @@
 import { Component } from '@angular/core';
 
+
+export interface producto{
+  nombre: string;
+  precio: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
 export class AppComponent {
-  title = 'AngularProyect';
+  title = 'ProyectoAngular';
+  contador = 0;
+  active = false;
+
+  tablaPrductos: producto [] = [
+    {nombre: 'Azucar', precio: 100},
+    {nombre: 'Galletas Bagley', precio: 80},
+    {nombre: 'Dulce de leche', precio: 120},
+    {nombre: 'Cafe molido', precio: 200},
+  ];
+
+  isactive() {
+    if(this.active) {
+      this.active = false;
+    }else{
+      this.active = true;
+    }
+  }
 }
